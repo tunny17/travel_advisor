549666e10aa34b5cb71eeec918c2500e444c5f32
tunny17 <tvnji17@gmail.com>
2023-02-07 20:55:03 +0100

    layout structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
+// material ui imports
+import { CssBaseline, Grid } from '@mui/material';
+
+// local component import
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
 function App() {
   return (
-    <div>
-      yam
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
 }
 
