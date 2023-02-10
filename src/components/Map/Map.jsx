1430a68f7b2052564b7ccd5c63e8dde51d29d5dd
tunny17 <tvnji17@gmail.com>
2023-02-10 20:05:01 +0100

    list component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import { LocationOnOutlined } from '@material-ui/icons';
+import { Rating } from '@mui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = {  lat: 0, lng: 0 };
+
   return (
-    <div>Map</div>
+    <div  className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyC2Inr4_TuMu2FIQy8-GRXgwSqjbyZLkNY' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
   )
 }
 
