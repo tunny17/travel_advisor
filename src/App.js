0d58cc4cae0029606a5a251ece8c0d233942a292
tunny17 <tvnji17@gmail.com>
2023-04-13 20:40:24 +0100

    - Passed each bounds and the places prop

--- src/App.js
+++ src/App.js
@@ -23,9 +23,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    console.log(coordinates, bounds);
-
-    getPlacesData()
+    getPlacesData(bounds.sw, bounds.ne)
       .then((data) => {
         console.log(data);
 
@@ -39,7 +37,7 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: '100%' }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
         <Grid item xs={12} md={8}>
           <Map 
--- src/api/index.js
+++ src/api/index.js
@@ -2,22 +2,20 @@ import axios from 'axios';
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
 
-const options = {
-    params: {
-      bl_latitude: '11.847676',
-      tr_latitude: '12.838442',
-      bl_longitude: '109.095887',
-      tr_longitude: '109.149359',
-    },
-    headers: {
-      'X-RapidAPI-Key': '07e0c981e1msh6327daf3bb5718fp114d20jsn37f2c00e482d',
-      'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-    }
-};
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
     try {
-        const { data : { data } } = await axios.get(URL, options);
+        const { data : { data } } = await axios.get(URL, {
+          params: {
+            bl_latitude: sw.lat,
+            tr_latitude: ne.lat,
+            bl_longitude: sw.lng,
+            tr_longitude: ne.lng,
+          },
+          headers: {
+            'X-RapidAPI-Key': '07e0c981e1msh6327daf3bb5718fp114d20jsn37f2c00e482d',
+            'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+          }
+        });
 
         return data;
     } catch (error) {
